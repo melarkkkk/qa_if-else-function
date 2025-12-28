53bdbbd21e4fee054827928373b00e48077b789f
melarkkkk <melark@192-168-100-126.lan>
2025-12-28 14:55:21 +0100

    add task solution

--- src/ifElse.test.js
+++ src/ifElse.test.js
@@ -1,11 +1,75 @@
 'use strict';
+/* eslint-disable max-len */
 
 describe('ifElse', () => {
-  // const { ifElse } = require('./ifElse');
+  const { ifElse } = require('./ifElse');
 
-  it('should ', () => {
-
+  it('should be declared', () => {
+    expect(ifElse).toBeInstanceOf(Function);
   });
 
-  // write tests here
+  it('should call the condition function exactly 1 time', () => {
+    const condition = jest.fn();
+    const first = jest.fn();
+    const second = jest.fn();
+
+    ifElse(condition, first, second);
+
+    expect(condition).toHaveBeenCalled();
+    expect(condition).toHaveBeenCalledTimes(1);
+  });
+
+  it('should call the first function exactly 1 time when condition returns true', () => {
+    const condition = jest.fn(() => true);
+    const first = jest.fn();
+    const second = jest.fn();
+
+    ifElse(condition, first, second);
+
+    expect(first).toHaveBeenCalled();
+    expect(first).toHaveBeenCalledTimes(1);
+  });
+
+  it('should not call second function when condition returns true', () => {
+    const condition = jest.fn(() => true);
+    const first = jest.fn();
+    const second = jest.fn();
+
+    ifElse(condition, first, second);
+
+    expect(first).toHaveBeenCalled();
+    expect(first).toHaveBeenCalledTimes(1);
+    expect(second).not.toHaveBeenCalled();
+  });
+
+  it('should call the second function exactly 1 time when condition returns false', () => {
+    const condition = jest.fn(() => false);
+    const first = jest.fn();
+    const second = jest.fn();
+
+    ifElse(condition, first, second);
+
+    expect(second).toHaveBeenCalled();
+    expect(second).toHaveBeenCalledTimes(1);
+  });
+
+  it('should not call first function when condition returns false', () => {
+    const condition = jest.fn(() => false);
+    const first = jest.fn();
+    const second = jest.fn();
+
+    ifElse(condition, first, second);
+
+    expect(second).toHaveBeenCalled();
+    expect(second).toHaveBeenCalledTimes(1);
+    expect(first).not.toHaveBeenCalled();
+  });
+
+  it('should not return anything', () => {
+    const condition = jest.fn(() => false);
+    const first = jest.fn();
+    const second = jest.fn();
+
+    expect(ifElse(condition, first, second)).toBeUndefined();
+  });
 });
